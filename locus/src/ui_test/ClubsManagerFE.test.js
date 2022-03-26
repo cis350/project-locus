9a961d23d7086ca554b901a256b492be7c934317
Dustin Fang <20fangd@gmail.com>
2022-03-26 15:43:44 -0400

    added back old testing

---
+++ locus/src/ui_test/ClubsManagerFE.test.js
@@ -0,0 +1,43 @@
+/**
+* @jest-environment jsdom
+*/
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+import Clubs from '../components/Clubs';
+
+const dummyClubs = {
+  'club1': {
+    master: 'James'
+  },
+  'club2': {
+    master: 'Dustin'
+  }
+};
+
+test('Clubs Displays Which Club Needs Work?', () => {
+  render(<Clubs props={ null } />);
+  const textElement = screen.getByText(/Which Club Needs Work?/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Clubs button using Master', () => {
+  render(<Clubs props={ null } />);
+  const buttonElements = screen.getByText(/Master: /i);
+  expect(buttonElements).toBeInTheDocument();
+});
+
+test('Clubs names showing', () => {
+  render(<Club props={ null }/>);
+  for (let i = 0; i < dummyClubs.length; i++) {
+    const clubName = dummyClubs[i];
+    const textElement = screen.getByText(clubName);
+    expect(textElement).toBeInTheDocument();
+  }
+})
+
+
+
+
---
+++ locus/src/ui_test/ProfileFE.test.js
@@ -0,0 +1,56 @@
+/**
+* @jest-environment jsdom
+*/
+
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
+
+import Profile from '../src/components/Profile';
+
+const user = {
+  id: 1, name: 'Dustin Fang', year: 2024, major: 'Computer Science', email: 'fdustin@seas.upenn.edu' 
+};
+
+test('member profile test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Member Profile');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('name test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Dustin Fang');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('year test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Year: 2024');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('major test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Major: Computer Science');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('email test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Email: fdustin@seas.upenn.edu');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('message test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Message');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('image test', () => {
+  render(< Profile id={user.id} />);
+  const linkElement = screen.getByText('Message');
+  expect(linkElement).toBeInTheDocument();
+});
