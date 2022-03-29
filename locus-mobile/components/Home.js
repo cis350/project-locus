0109641ec54368b54c9d1a7a50dd9af011b1e879
Jeffroyang <jeffroyang@gmail.com>
2022-03-28 22:02:17 -0400

    updated logout button color on homepage

--- locus-mobile/components/Home.js
+++ locus-mobile/components/Home.js
@@ -5,22 +5,6 @@ import {
 import Profile from './Profile';
 // import { getUserFullName } from '../modules/storage';
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: '#6A9B72',
-    borderRadius: 10,
-    paddingVertical: 10,
-    width: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginHorizontal: 10,
-  },
-});
-
 export default function Home({ navigation }) {
   function handleLogout() {
     navigation.navigate('Main');
@@ -29,9 +13,25 @@ export default function Home({ navigation }) {
   return (
     <View style={styles.container}>
       <Profile />
-      <TouchableHighlight style={styles.button} onPress={() => handleLogout()} underlayColor="#33E86F">
-        <Text>Logout</Text>
+      <TouchableHighlight style={styles.button} onPress={() => handleLogout()} underlayColor="#b00017">
+        <Text style={{ textAlign: 'center', fontSize: 30 }}>Logout</Text>
       </TouchableHighlight>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#ff0022',
+    borderRadius: 10,
+    paddingVertical: 10,
+    width: 150,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+});
