91400218df170ae0595a103a8c94cc50a4e65918
Jeffroyang <jeffroyang@gmail.com>
2022-04-01 01:32:55 -0400

    changed status bar color

--- locus-mobile/App.js
+++ locus-mobile/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { StatusBar } from 'expo-status-bar';
 import Main from './components/Main';
 import Login from './components/Login';
 import Navigation from './components/Navigation';
@@ -11,7 +11,8 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Main" >
+      <StatusBar barStyle="dark-content" />
+      <Stack.Navigator initialRouteName="Main">
         <Stack.Screen name="Main" component={Main} options={{ title: 'LOCUS' }} />
         <Stack.Screen name="Login" component={Login} options={{ title: 'Login' }} />
         <Stack.Screen name="AppNavigation" component={Navigation} options={{ title: 'LOCUS', headerShown: false }} />
@@ -19,4 +20,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
