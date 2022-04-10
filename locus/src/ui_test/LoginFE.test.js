30e11ef4bcd6f8b800b79169560cf2592b5c9736
Jeffroyang <82118995+Jeffroyang@users.noreply.github.com>
2022-04-10 17:55:58 -0400

    mocked functions and fixed test cases
    Mocked useNavigate and storage functions so that LoginFE, ProfileFE, and RegisterFE tests can run.

--- locus/src/ui_test/LoginFE.test.js
+++ locus/src/ui_test/LoginFE.test.js
@@ -5,29 +5,36 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
 import Login from '../components/Login';
+import renderer from 'react-test-renderer';
+
+// mock useNavigate: https://github.com/remix-run/react-router/issues/7811
+const mockedNavigator = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+ useNavigate: () => mockedNavigator,
+}));
 
 test('Displays Log-in text', () => {
-  render(<Login setIsLoggedIn={() => null} setUserEmail={() => null} />);
-  const textElement = screen.getByText(/Log-in/i);
-  expect(textElement).toBeInTheDocument();
+  render(<Login />);
+  const textElement = screen.getAllByText(/Log-in/i);
+  expect(textElement[0]).toBeInTheDocument();
 });
 
 test('Displays Email', () => {
-  render(<Login setIsLoggedIn={() => null} setUserEmail={() => null} />);
+  render(<Login />);
   const textElement = screen.getByText(/Email/i);
   expect(textElement).toBeInTheDocument();
 });
 
 test('Displays Password', () => {
-  render(<Login setIsLoggedIn={() => null} setUserEmail={() => null} />);
+  render(<Login />);
   const textElement = screen.getByText(/Password/i);
   expect(textElement).toBeInTheDocument();
 });
 
-test('Displays Locus', () => {
-  render(<Login setIsLoggedIn={() => null} setUserEmail={() => null} />);
-  const textElement = screen.getByText(/Locus/i);
-  expect(textElement).toBeInTheDocument();
+test('Snapshot Test', () => {
+  const component = renderer.create(<Login />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- locus/src/ui_test/RegisterFE.test.js
+++ locus/src/ui_test/RegisterFE.test.js
@@ -5,19 +5,26 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
+import renderer from 'react-test-renderer';
 import Register from '../components/Register';
 
-test('Displays Locus', () => {
-  render(<Register setJustRegistered={() => null} />);
-  const textElement = screen.getByText(/Locus/i);
-  expect(textElement).toBeInTheDocument();
-});
+// mock useNavigate: https://github.com/remix-run/react-router/issues/7811
+const mockedNavigator = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+ useNavigate: () => mockedNavigator,
+}));
 
 test('Displays Register title', () => {
   render(<Register setJustRegistered={() => null} />);
-  const textElement = screen.getByText(/Register/i);
-  expect(textElement).toBeInTheDocument();
+  const textElement = screen.getAllByText(/Register/i);
+  expect(textElement[0]).toBeInTheDocument();
+});
+
+test('Displays Register Button', () => {
+  render(<Register setJustRegistered={() => null} />);
+  const textElement = screen.getAllByText(/Register/i);
+  expect(textElement[1]).toBeInTheDocument();
 });
 
 test('Displays Email', () => {
@@ -28,6 +35,18 @@ test('Displays Email', () => {
 
 test('Displays Password', () => {
   render(<Register setJustRegistered={() => null} />);
-  const textElement = screen.getByText(/Password/i);
+  const textElement = screen.getByText('Password');
   expect(textElement).toBeInTheDocument();
+});
+
+test('Displays Verify Password', () => {
+  render(<Register setJustRegistered={() => null} />);
+  const textElement = screen.getByText(/Verify Password/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Snapshot Test', () => {
+  const component = renderer.create(<Register />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
