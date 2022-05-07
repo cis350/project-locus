9ae89a0d33207d3044b3d4defb8c94785c65e347
Zexerf <76970913+Zexerf@users.noreply.github.com>
2022-05-07 17:40:12 -0400

    login, main, nav frontend test

--- locus/src/frontend_tests/LoginFE.test.js
+++ locus/src/frontend_tests/LoginFE.test.js
@@ -5,36 +5,72 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import Login from '../components/Login';
-import renderer from 'react-test-renderer';
 
 // mock useNavigate: https://github.com/remix-run/react-router/issues/7811
 const mockedNavigator = jest.fn();
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
- useNavigate: () => mockedNavigator,
+  useNavigate: () => mockedNavigator,
 }));
 
-test('Displays Log-in text', () => {
-  render(<Login />);
-  const textElement = screen.getAllByText(/Log-in/i);
-  expect(textElement[0]).toBeInTheDocument();
+describe('Initial Login page displays', () => {
+  test('Displays Log-in text', () => {
+    render(<Login />);
+    const textElement = screen.getAllByText(/Log-in/i);
+    expect(textElement[0]).toBeInTheDocument();
+  });
+  test('Displays Email', () => {
+    render(<Login />);
+    const textElement = screen.getByText(/Email/i);
+    expect(textElement).toBeInTheDocument();
+  });
+  test('Displays Password', () => {
+    render(<Login />);
+    const textElement = screen.getByLabelText(/Password/i);
+    expect(textElement).toBeInTheDocument();
+  });
+  test('Displays log-in button', () => {
+    render(<Login />);
+    const button = screen.getByRole('button', { name: /Log-in/i });
+    expect(button).toBeInTheDocument();
+  });
+  test('Displays reset password button', () => {
+    render(<Login />);
+    const button = screen.getByRole('button', { name: /reset/i });
+    expect(button).toBeInTheDocument();
+  });
 });
 
-test('Displays Email', () => {
-  render(<Login />);
-  const textElement = screen.getByText(/Email/i);
-  expect(textElement).toBeInTheDocument();
-});
-
-test('Displays Password', () => {
-  render(<Login />);
-  const textElement = screen.getByText(/Password/i);
-  expect(textElement).toBeInTheDocument();
-});
-
-test('Snapshot Test', () => {
-  const component = renderer.create(<Login />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('Alert message tests', () => {
+  test('empty fields', () => {
+    render(<Login />);
+    userEvent.click(screen.getByRole('button', { name: /Log-in/i }));
+    const alertElement = screen.getByText('Please enter both your email and password.');
+    expect(alertElement).toBeInTheDocument();
+  });
+  test('password field empty', () => {
+    render(<Login />);
+    userEvent.type(screen.getByTestId('email-input'), 'test@gmail.com');
+    userEvent.click(screen.getByRole('button', { name: /Log-in/i }));
+    const alertElement = screen.getByText('Please enter both your email and password.');
+    expect(alertElement).toBeInTheDocument();
+  });
+  test('email field empty', () => {
+    render(<Login />);
+    userEvent.type(screen.getByTestId('password-input'), 'pass');
+    userEvent.click(screen.getByRole('button', { name: /Log-in/i }));
+    const alertElement = screen.getByText('Please enter both your email and password.');
+    expect(alertElement).toBeInTheDocument();
+    userEvent.click(screen.getByRole('button', { name: /reset/i }));
+  });
+  // test('invalid login', () => {
+  //   render(<Login />);
+  //   userEvent.type(screen.getByTestId('email-input'), 'test@gmail.com');
+  //   userEvent.type(screen.getByTestId('password-input'), 'pass');
+  //   userEvent.click(screen.getByRole('button', { name: /Log-in/i }));
+  //   const alertElement = screen.getByText('Email or/and password invalid.');
+  //   expect(alertElement).toBeInTheDocument();
+  // });
 });
